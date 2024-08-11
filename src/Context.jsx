6998f56a460f7652b883818ff49f5ad63d91d4ab
Done83 <nedomehan@gmail.com>
2024-08-11 10:36:37 +0200

    A: global context and custom hook completed

---
+++ src/Context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSidebarOpen(false);
+  };
+  return (
+    <AppContext.Provider value={{ isSidebarOpen, openSideBar, closeSideBar }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
